0e16aa0c7db2dee4d08ce5d1bfb3fd8b387c4b22
Gabriel V. Santana <gabrielsantana.it@gmail.com>
2023-12-09 11:02:27 -0300

    refactor: change output to folders per pdf instead of only files

--- src/main.ts
+++ src/main.ts
@@ -1,123 +1,110 @@
 import { execSync } from 'child_process';
-import { mkdir, readFile, readdir, rm, rmdir, unlink, writeFile } from 'fs/promises';
-import { join } from 'path';
+import { mkdir, readFile, readdir, rm, unlink, writeFile } from 'fs/promises';
+import { join, parse } from 'path';
 import { PDFDocument } from 'pdf-lib';
 
 import { logger } from './logger';
 
 const env = {
+  fileName: 'nest' ?? 'page',
   input: `${__dirname}/../_input_`,
   output: `${__dirname}/../_output_`,
   temp: `${__dirname}/../_temp_`,
   drawio: '/Applications/draw.io.app/Contents/MacOS/draw.io',
 };
 
-const convertDrawioToPdf = async (inputDir: string, outputDir: string, drawioPath: string) => {
-  const files = await readdir(inputDir);
-  const drawioFiles = files.filter((file) => file.endsWith('.drawio') || file.endsWith('.xml'));
+const getFilesFromDir = async (dir: string, extensions: string[]) => {
+  const files = await readdir(dir);
+  return files.filter((file) => extensions.some((ext) => file.endsWith(ext)));
+};
 
-  for (const file of drawioFiles) {
-    const inputFile = `${inputDir}/${file}`;
-    const outputFolder = `${outputDir}/`;
-
-    try {
-      execSync(`${drawioPath} -a --crop -x -o "${outputFolder}" "${inputFile}"`);
-      logger.info(`PDF generated for "${file}"`);
-    } catch (error) {
-      await rmdir(outputFolder);
-      logger.error(`Error generating PDF file for "${file}":`, error);
-      throw error;
-    }
+const execCommand = (command: string, errorMessage: string) => {
+  try {
+    execSync(command);
+  } catch (error) {
+    logger.error(errorMessage, error);
+    throw error;
   }
 };
 
-const mergePdfFiles = async (outputDir: string) => {
-  const files = await readdir(outputDir);
-  const pdfFiles = files.filter((file) => file.endsWith('.pdf'));
-
-  const pdfDoc = await PDFDocument.create();
-
-  for (const pdfFile of pdfFiles) {
-    const pdfPath = join(outputDir, pdfFile);
-    const pdfBytes = await readFile(pdfPath);
-    const pdfDocToMerge = await PDFDocument.load(pdfBytes);
-    const pageCount = pdfDocToMerge.getPageCount();
-
-    for (let i = 0; i < pageCount; i++) {
-      const [copiedPage] = await pdfDoc.copyPages(pdfDocToMerge, [i]);
-      pdfDoc.addPage(copiedPage);
-    }
-  }
-
-  const pdfBytes = await pdfDoc.save();
-  await writeFile(`${outputDir}/merged.pdf`, pdfBytes);
+const convertDrawioToPdf = async (inputDir: string, outputDir: string, drawioPath: string) => {
+  const drawioFiles = await getFilesFromDir(inputDir, ['.drawio', '.xml']);
 
   await Promise.all(
-    pdfFiles.map((pdfFile) => {
-      const filePath = join(outputDir, pdfFile);
-      return unlink(filePath);
+    drawioFiles.map(async (file) => {
+      const inputFile = `${inputDir}/${file}`;
+      const outputFolder = `${outputDir}/`;
+      const command = `${drawioPath} -a --crop -x -o "${outputFolder}" "${inputFile}"`;
+      execCommand(command, `Error generating PDF file for "${file}":`);
+      logger.info(`PDF generated for "${file}"`);
     }),
   );
 };
 
-const splitPdfFiles = async (input: string) => {
-  const pdfBytes = await readFile(`${input}/merged.pdf`);
-  const pdfDoc = await PDFDocument.load(pdfBytes);
+const splitPdfFiles = async (inputDir: string, outputDir: string) => {
+  const pdfFiles = await getFilesFromDir(inputDir, ['.pdf']);
 
-  const pageCount = pdfDoc.getPageCount();
-
-  for (let i = 0; i < pageCount; i++) {
-    const newPdfDoc = await PDFDocument.create();
-    const [page] = await newPdfDoc.copyPages(pdfDoc, [i]);
-    newPdfDoc.addPage(page);
-    const newPdfBytes = await newPdfDoc.save();
-
-    await writeFile(join(input, `page${i + 1}.pdf`), newPdfBytes);
-  }
-
-  await unlink(join(input, 'merged.pdf'));
+  await Promise.all(
+    pdfFiles.map(async (file) => {
+      const { base, name: folderName } = parse(file);
+      const newPath = join(outputDir, folderName);
+      await mkdir(newPath, { recursive: true });
+      await splitPdf(inputDir, newPath, base);
+      await convertPdfToSvg(newPath);
+    }),
+  );
 };
 
-const convertPdfToSvg = async (inputDir: string, outputDir: string) => {
-  const files = await readdir(inputDir);
-  const pdfFiles = files.filter((file) => file.endsWith('.pdf'));
+const splitPdf = async (inputDir: string, outputDir: string, fileName: string) => {
+  const pdfBytes = await readFile(join(inputDir, fileName));
+  const pdfDoc = await PDFDocument.load(pdfBytes);
+  const pageCount = pdfDoc.getPageCount();
 
-  for (const file of pdfFiles) {
-    const inputFile = `${inputDir}/${file}`;
-    const outputFile = `${outputDir}/${file.replace('.pdf', '.svg')}`;
+  await Promise.all(
+    Array.from({ length: pageCount }).map(async (_, i) => {
+      const newPdfDoc = await PDFDocument.create();
+      const [page] = await newPdfDoc.copyPages(pdfDoc, [i]);
+      newPdfDoc.addPage(page);
+      const newPdfBytes = await newPdfDoc.save();
+      return writeFile(join(outputDir, `${env.fileName}-${i + 1}.pdf`), newPdfBytes);
+    }),
+  );
 
-    try {
-      execSync(`inkscape --without-gui --file="${inputFile}" --export-plain-svg="${outputFile}"`);
+  await unlink(join(inputDir, fileName));
+};
+
+const convertPdfToSvg = async (inputDir: string) => {
+  const pdfFiles = await getFilesFromDir(inputDir, ['.pdf']);
+
+  await Promise.all(
+    pdfFiles.map(async (file) => {
+      const inputFile = `${inputDir}/${file}`;
+      const outputFile = `${inputDir}/${file.replace('.pdf', '.svg')}`;
+      const command = `inkscape ${inputFile} --export-filename=${outputFile}`;
+      execCommand(command, `Error generating SVG file for "${file}":`);
+      unlink(inputFile);
       logger.info(`SVG generated for "${file}"`);
-    } catch (error) {
-      logger.error(`Error generating SVG file for "${file}":`, error);
-      throw error;
-    }
-  }
+    }),
+  );
 };
 
 const main = async () => {
   await mkdir(env.temp, { recursive: true });
 
-  logger.info('Step 1/4 :: convertDrawioToPdf...');
+  logger.info('Step 1/3 :: convertDrawioToPdf...');
   await convertDrawioToPdf(env.input, env.temp, env.drawio);
-  logger.info('Step 1/4 :: convertDrawioToPdf done!');
+  logger.info('Step 1/3 :: convertDrawioToPdf done!');
 
-  logger.info('Step 2/4 :: mergePdfFiles...');
-  await mergePdfFiles(env.temp);
-  logger.info('Step 2/4 :: mergePdfFiles done!');
+  logger.info('Step 2/3 :: splitPdfFiles...');
+  await splitPdfFiles(env.temp, env.output);
+  logger.info('Step 2/3 :: splitPdfFiles done!');
 
-  logger.info('Step 3/4 :: splitPdfFiles...');
-  await splitPdfFiles(env.temp);
-  logger.info('Step 3/4 :: splitPdfFiles done!');
-
-  logger.info('Step 4/4 :: convertPdfToSvg...');
-  await convertPdfToSvg(env.temp, env.output);
-  logger.info('Step 4/4 :: convertPdfToSvg done!');
+  logger.info('Step 3/3 :: convertPdfToSvg...');
+  await convertPdfToSvg(env.output);
+  logger.info('Step 3/3 :: convertPdfToSvg done!');
 
   logger.info('All steps completed!');
+  await rm(env.temp, { recursive: true, force: true });
 };
 
-main()
-  .catch(logger.error)
-  .finally(() => rm(env.temp, { recursive: true, force: true }));
+main().catch(logger.error);
